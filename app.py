214c064b9ccd7bdafa1810023eb3a2de47df04b4
shar4440 <sharanayyahiremath76@gmail.com>
2025-08-02 11:02:43 +0530

    gunicorn for deployment

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import pandas as pd
 #app starts
 app = Flask(__name__)
 
-##Load the pickle model
 
+##Load the pickle model
 regmodel = pickle.load(open('linear_regression.pkl','rb'))
 scalar = pickle.load(open('scaling.pkl','rb'))
 
@@ -17,14 +17,14 @@ scalar = pickle.load(open('scaling.pkl','rb'))
 def home():
   return render_template('home.html')
 
+
+
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
   data = request.json['data']
   print(data)
   print(np.array(list(data.values())).reshape(1,-1))
-
   new_transfered_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
-
   output = regmodel.predict(new_transfered_data)
   print(output[0])
   return jsonify(output[0])
@@ -38,13 +38,9 @@ def predict_form():
   data = [float(x) for x in request.form.values()]
   #this final output will contain the scaled data or input  values
   final_input = scalar.transform(np.array(data).reshape(1,-1))
-
   print(final_input)
   output = regmodel.predict(final_input)
   return render_template("home.html",prediction_text="The House prediction price is {} ".format(output))
-
-
-
 
 
 
