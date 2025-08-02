a87cb28a7ca6faeaf3e67b637ff55e08258bf4fe
shar4440 <sharanayyahiremath76@gmail.com>
2025-08-02 10:17:39 +0530

    web api commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import pickle
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+
+#app starts
+app = Flask(__name__)
+
+##Load the pickle model
+
+regmodel = pickle.load(open('linear_regression.pkl','rb'))
+scalar = pickle.load(open('scaling.pkl','rb'))
+
+
+@app.route('/')
+def home():
+  return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+  data = request.json['data']
+  print(data)
+  print(np.array(list(data.values())).reshape(1,-1))
+
+  new_transfered_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
+
+  output = regmodel.predict(new_transfered_data)
+  print(output[0])
+  return jsonify(output[0])
+if __name__ == "__main__":
+  app.run(debug=True)
